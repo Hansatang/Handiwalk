22b7af87319aa7daeb0201c5dd9488c43fac2a6d
Hansatang <portokarlos@gmail.com>
2022-04-25 10:02:14 +0200

    Initial navigation drawer

--- app/src/main/java/com/example/handiwalk/LoginActivity.java
+++ app/src/main/java/com/example/handiwalk/LoginActivity.java
@@ -7,16 +7,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -24,6 +30,14 @@ public class LoginActivity extends AppCompatActivity {
     EditText PasswordField;
     Button loginButton;
     Button toRegisterButton;
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == RESULT_OK)
+                    goToMain();
+                else
+                    Toast.makeText(this, "SIGN IN CANCELLED", Toast.LENGTH_SHORT).show();
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,8 +87,17 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void goToRegister() {
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setLogo(R.drawable.ic_launcher_foreground)
+                .build();
+
+        activityResultLauncher.launch(signInIntent);
     }
 
 }
