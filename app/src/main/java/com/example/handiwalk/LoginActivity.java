6b29e8d12aaebdc8b227e7c3f95b97d0051d1114
Hansatang <portokarlos@gmail.com>
2022-04-25 09:50:44 +0200

    Initial navigation drawer

---
+++ app/src/main/java/com/example/handiwalk/LoginActivity.java
@@ -0,0 +1,80 @@
+package com.example.handiwalk;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    EditText UsernameField;
+    EditText PasswordField;
+    Button loginButton;
+    Button toRegisterButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        findViews();
+        setListenersToButtons();
+    }
+
+    private void findViews() {
+        UsernameField = findViewById(R.id.UsernameField);
+        PasswordField = findViewById(R.id.PasswordField);
+        loginButton = findViewById(R.id.LoginButton);
+        toRegisterButton = findViewById(R.id.toRegisterView);
+    }
+
+    private void setListenersToButtons() {
+        loginButton.setOnClickListener(
+                view -> login(UsernameField.getText().toString(), PasswordField.getText().toString())
+        );
+        toRegisterButton.setOnClickListener(
+                view -> goToRegister());
+    }
+
+    private void login(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToMain();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void goToMain() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    private void goToRegister() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+}
