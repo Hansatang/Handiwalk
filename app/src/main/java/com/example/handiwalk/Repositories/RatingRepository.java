ab147fa8b2027e5a6a2d8c1e0f860c35a4c2eaec
Fogh1206 <esbenvensel.fogh@gmail.com>
2022-05-11 12:27:10 +0200

    Changing from Maps to numbers

--- app/src/main/java/com/example/handiwalk/Repositories/RatingRepository.java
+++ app/src/main/java/com/example/handiwalk/Repositories/RatingRepository.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
@@ -24,7 +25,9 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -54,14 +57,20 @@ public class RatingRepository {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         FirebaseUser userLoggedIn = FirebaseAuth.getInstance().getCurrentUser();
         ReviewModel reviewModel = new ReviewModel(userLoggedIn.getUid(), rating);
-        DocumentReference reference = db.collection("locations").document(locationModel.getId()+"");
-        reference.update("Reviews", FieldValue.arrayUnion(reviewModel.getReview())).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                System.out.println("You rated " + locationModel.getName() + " to be " + rating + " stars");
-            }
-        });
 
+        DocumentReference reference = db.collection("locations").document(locationModel.getId()+"");
+
+        reference.update("Reviews", FieldValue.arrayUnion(rating)).addOnCompleteListener(arrayUnionTask -> {
+            reference.get().addOnCompleteListener(getDocumentTask -> {
+
+                DocumentSnapshot document = getDocumentTask.getResult();
+                ArrayList<Float> reviews = (ArrayList<Float>) document.getData().get("Reviews");
+
+                System.out.println(reviews.get(1) +"!!!!!!!");
+
+            });
+
+        });
     }
 
     public LiveData<List<ReviewModel>> getReviewLiveData() {
